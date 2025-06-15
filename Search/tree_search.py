98a09d72ddfc483e404ad5ba1743cccbf0559e4f
GameLaxx <tmboisse@gmail.com>
2025-06-15 12:38:06 +0200

    First optimizations

--- Search/tree_search.py
+++ Search/tree_search.py
@@ -12,6 +12,8 @@ def represent_board(board, position):
         ret += "."
     return ret
 
+precomputed_coords = [(i // 8, i % 8) for i in range(64)]
+
 class Node():
     def __init__(self, board, position, parent = None):
         self.board = board
@@ -20,12 +22,12 @@ class Node():
         self.parent = parent
 
     def generate_childs(self):
-        current_square = divmod(self.position, 8)
+        current_square = precomputed_coords[self.position]
         for delta in [6, 15, 10, 17, -6, -15, -10, -17]:
             new_position = self.position + delta
             if not (0 <= new_position < 64): # not on the board
                 continue
-            wanted_square = divmod(new_position, 8)
+            wanted_square = precomputed_coords[new_position]
             if not (abs(wanted_square[0] - current_square[0]) <= 2 and abs(wanted_square[1] - current_square[1]) <= 2): # not around the knight
                 continue
             if self.board & 1 << new_position != 0: # already been there
@@ -38,7 +40,7 @@ class Node():
 class Tree():
     def __init__(self, start_node):
         self.start_node = start_node
-        self.visited = {}
+        self.visited = set()        
         self.total_searchs = 0
 
     def __search(self, current_node : Node):
@@ -47,7 +49,7 @@ class Tree():
             return None
         if current_node.board == 0xFFFFFFFFFFFFFFFF: #complete
             return current_node
-        if current_node.board in self.visited and current_node.position in self.visited[current_node.board] and not self.visited[current_node.board][current_node.position]:
+        if (current_node.board, current_node.position) in self.visited:
             return None # already seen and failed
         current_node.generate_childs()
         for child in current_node.childs:
@@ -55,9 +57,7 @@ class Tree():
             if ret_search: # found complete
                 return ret_search
         # no child is worth
-        if current_node.board not in self.visited:
-            self.visited[current_node.board] = {}
-        self.visited[current_node.board][current_node.position] = False
+        self.visited.add((current_node.board, current_node.position))
         return None
         
     def launch_search(self):
